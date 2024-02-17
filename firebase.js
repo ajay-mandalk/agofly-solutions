cf8683c343413ddb8285a584a6d9969909ad333f
Ajay-1998-mandal <153422738+Ajay-1998-mandal@users.noreply.github.com>
2024-02-17 22:24:05 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzdWFDYJDCLWJUiXOgF8J95L9G7IrBxI8",
+  authDomain: "agrofly-2e396.firebaseapp.com",
+  projectId: "agrofly-2e396",
+  storageBucket: "agrofly-2e396.appspot.com",
+  messagingSenderId: "1052887433461",
+  appId: "1:1052887433461:web:7618036fbc910e220f05bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
